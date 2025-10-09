0b4d0c8947794abdcf647accbb78cd3f4ae747c4
hamza110376 <muhammadhamzaawan110@gmail.com>
2025-10-09 06:50:04 +0500

    done till exception

---
+++ basics/dictionaay.py
@@ -0,0 +1,42 @@
+# merge 2 dictionaries
+
+# d1={10:30, 20:40, 40:50}
+# d2={50:10, 60:20, 70:30}
+
+# for i in d2:
+#     d1[i]= d2[i]
+
+# print(d1)
+
+
+# sum all the values
+
+# d1={10:30, 20:40, 40:50}
+# sum = 0
+# for i in d1:
+#     sum = sum+d1[i]
+# print(sum)
+
+# count frequency
+
+# a = [1,1,1,2,2,2,3,3,3,4,4,4,5,5,6,7,8]
+
+# d={}
+
+# for i in a:
+#     if i in d.keys():
+#         d[i]+=1
+#     else:
+#          d[i]=1
+# print(d)
+
+
+# d1 = {10:100,20:200,40:300}
+# d2 = {40:400,50:500,60:600}
+
+# for i in d2:
+#     if i in d1.keys():
+#         d1[i] += d2[i]
+#     else:
+#          d1[i] = d2[i]
+# print(d1)
---
+++ basics/exceptions.py
@@ -0,0 +1,24 @@
+# a= int(input("please enter a number to divide: "))
+
+# try:
+#     print(10/a)
+# except Exception as err :
+#     print("there will be error")
+# else:
+#     print("good there is no exception")
+# finally:
+#     print("I will run no matter what")
+
+# print("divided successfully")
+
+
+# age= int(input("enter your age: "))
+
+# try:
+#     if age<10 or age>18:
+#         raise ValueError("your age must be between 10 and 18")
+#     else:
+#         print("welcome to the club")
+# except Exception as err:
+#     print(f"an error occurred as {err}")
+# print("the club will start soon")
